7d3c0ad91940122ce92abb1cd1ad65903fef85cb
Vilizar Denichin <denichinv@gmail.com>
2025-01-12 17:10:32 +0000

    card component created

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import Card from "./components/Card"
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <>
+    <h1>Explore Our Holiday Sale Now!</h1>
+    <Card title={"Beach Paradise"} description={"Relax on golden sands and enjoy crystal-clear waters. Your perfect vacation awaits!"} buttonText={"Book Now"} imageUrl={"https://2cw.co.uk/th279/images/1+192ea2-paradise-beach-hotel-koh-samui.jpg"}/>
+    </>
+  )
+}
+
+export default App
